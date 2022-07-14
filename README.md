63facbd999db6f7529cb9563340d2f34c5014326
czh0923 <zchenggn@126.com>
2022-07-13 20:50:00 -0700

    creating new workspace, icon and title

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # chrome_extension
+
+1. https://www.youtube.com/watch?v=0n809nd4Zu4
--- hetui/inhale.js
+++ hetui/inhale.js
@@ -1,22 +1,69 @@
 const inhaleButton = document.getElementById("inhaleButton");
-console.log("in inhale.js", inhaleButton);
+const workspacesDiv = document.getElementById("workspacesDiv");
+console.log("in inhale.js");
 
-function inhale() {
-    console.log("hello");
-    // console.log(chrome.tabs);
-    // chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-    //     console.log(tabs[0].url, tabs[1].url);
-    // });
-    chrome.windows.getCurrent({"populate":true}, function(currentWindow) {
-        var tabURLs = [];
-        var tabs = currentWindow.tabs;
-        console.log(tabs);
-        for (var i=0; i<tabs.length; i++) {
-            tabURLs.push(tabs[i].url);
-        }
-        console.log(tabURLs);
-     });
+function createNewWorkspace() {
+    let div = document.createElement("div");
+    div.className = "workspace";
+    div.innerText = "workspace";
+
+    workspacesDiv.appendChild(div);
+
+    return div;
+}
+
+function display(div, tabIconURLs, tabTitles) {
+
+    console.log("here", tabIconURLs, tabTitles, tabTitles[0], tabTitles.length);
+
+    for (let i = 0; i < tabTitles.length; i++) {
+        console.log(tabIconURLs[i], tabTitles[i]);
+
+        let p = document.createElement("p");
+
+        let icon = document.createElement("img");
+        icon.src = tabIconURLs[i];
+
+        let innerP = document.createElement("p");
+        innerP.className = "inline-p";
+        innerP.innerHTML = tabTitles[i];
+
+        p.appendChild(icon);
+        p.appendChild(innerP);
+
+        div.appendChild(p);
+    }
+}
+
+async function getTabs() {
+
+    const tabURLs = [];
+    const tabIconURLs = [];
+    const tabTitles = [];
+
+    const tabs = await chrome.windows.getCurrent({"populate":true});
+
+    console.log(tabs.tabs);
+
+    tabs.tabs.forEach(function(tab) {
+        tabURLs.push(tab.url);
+        tabIconURLs.push(tab.favIconUrl);
+        tabTitles.push(tab.title);
+    })
     
+    return {tabIconURLs, tabTitles};
+}
+
+async function inhale() {
+    console.log("hello");
+
+    let newWorkspaceDiv = createNewWorkspace();
+    console.log(newWorkspaceDiv);
+
+    let {tabIconURLs, tabTitles} = await getTabs();
+
+    display(newWorkspaceDiv, tabIconURLs, tabTitles);
+
 }
 
 inhaleButton.addEventListener("click", inhale)
